d41d41cb467b1d83ac5fbe26772e7eb13e8e502f
Kaarel.R <krais@schmidtocean.org>
2026-05-02 19:30:55 -0300

    UI - templates - make warning red

--- src/components/create_event_template.js
+++ src/components/create_event_template.js
@@ -274,7 +274,7 @@ class CreateEventTemplate extends Component {
                   sm={12}
                 />
                 <div className="col-12" style={{marginTop: '-10px', marginBottom: '10px'}}>
-                  <small className="text-muted">
+                  <small className="text-danger">
                     Button Name is not included in data
                   </small>
                 </div>
